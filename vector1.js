107786ae173f60a748bdce09349b2d710d6c9868
rgaviriav <rgaviriav@unicartagena.edu.co>
2025-09-10 07:55:45 -0500

    cambio 3

--- vector1.js
+++ vector1.js
@@ -9,6 +9,42 @@ for (let i = 0; i < vertorEnteros.length; i++) {
     vertorEnteros[i] = Math.floor(Math.random() * 100);
 }
 
+console.log("---- vector de 10 enteros----")
 console.log(vertorEnteros);
 // refrescar la pestaña sin cerrar la consolo para que aparezca el vector
 
+//recorrer el vector usando el metodo for-each (for...of)
+
+    console.log("---- metodo for - each ----");
+
+for(let valor of vertorEnteros){
+
+    console.log(valor);
+}
+
+//cambiar los valores impares por ceros
+
+    console.log("---- convertir impares en ceros ----")
+
+for (let i = 0; i < vertorEnteros.length; i++) {
+    if(vertorEnteros[i] % 2 !== 0){
+        vertorEnteros[i] = 0;
+    }
+    
+}
+
+    console.log(vertorEnteros);
+
+//multiplicar todos los valores por el valor de su indice.
+
+    console.log("----- multiplicación de todos los valores por su indice ----")
+
+    for (let i = 0; i < vertorEnteros.length; i++) {
+         vertorEnteros[i] = vertorEnteros[i] * i;
+    
+        
+    }
+    
+    console.log(vertorEnteros);
+
+//busqueda lineal de un valor.
